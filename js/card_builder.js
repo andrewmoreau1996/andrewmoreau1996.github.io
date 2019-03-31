dd3592121c74f67f673751387ba8226d1580946d
Andrew Moreau <andrewmoreau1996@gmail.com>
2019-03-31 17:57:33 -0400

    added an id

--- js/card_builder.js
+++ js/card_builder.js
@@ -1,10 +1,12 @@
-function build_project_card(name, image, alt){
+function build_project_card(name, image, alt, id){
+    //here the id will be the index in the projects array     
     var card = '<div class="col-md-6 mt-5">'+
-                    '<div class="card project">'+
+                    '<div class="card project" id="' + id + '">'+
                         '<img class="card-img-top" src="' + image + '" alt="' + alt + '">'+
                         '<div class="card-body">'+
                             '<h4 class="card-text">' + name + '</h4>'+
                         '</div>'+
                     '</div>'+
                 '</div>';
+    return card;
 }
